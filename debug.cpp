7a14eb0f1d3f50ce3190b5479c2be062ef33eef8
si1lverfish <vandras2003@gmail.com>
2020-08-25 15:15:06 +0200

    debug.cpp added

--- debug.cpp
+++ debug.cpp
@@ -1,33 +1,48 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-#define ull unsigned long long
-#define ll long long
-#define pb push_back
-#define pii pair<int, int>
-#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define TC int tc; cin >> tc; while(tc--)
-
+/*<DEBUG>*/
+#define tem template <typename 
+#define can_shift(_X_, ...) enable_if_t<sizeof test<_X_>(0) __VA_ARGS__ 8, debug&> operator<<(T i)
+#define op debug& operator<<
+tem C > auto test(C *x) -> decltype(cerr << *x, 0LL);
+tem C > char test(...);
+tem C > struct itr{C begin, end; };
+tem C > itr<C> get_range(C b, C e) { return itr<C>{b, e}; };
 struct debug{
 #ifdef _LOCAL
 	~debug(){ cerr << endl; }
-
+	tem T > can_shift(T, ==){ cerr << boolalpha << i; return *this; }
+	tem T> can_shift(T, !=){ return *this << get_range(begin(i), end(i)); }
+	tem T, typename U > op (pair<T, U> i){ 
+		return *this << "< " << i.first << " , " << i.second << " >"; }
+	tem T> op (itr<T> i){
+		*this <<  "{ ";
+		for(auto it = i.begin; it != i.end; it++){
+			*this << " , " + (it==i.begin?2:0) << *it;
+		}
+		return *this << " }";
+	}
 #else
-
-#endif
+tem T> op (const T&) { return *this; }
+#endif 
 };
 
-using namespace std;
+#define expand(...) " [ " << #__VA_ARGS__ << " : " << (__VA_ARGS__) << " ]"
+/*</DEBUG>*/
 
-#define expand(...) " [" << #__VA_ARGS__ << " : " << (__VA_ARGS__) << " ]"
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define TC int __TC__; cin >> __TC__; while(__TC__--)
 
 const int INF = 1e9 + 7;
 
 int main(void)
 {
 	FAST;
-	int a = 10, b = 12;
-	int animal = 10, africa = 20;
-	cerr << expand(a)  << expand(b) << endl;
-
 	return 0;
 }
