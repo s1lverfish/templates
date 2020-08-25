ff4cbea914a1bc4df4342ba7a28859b009b58ca7
s1lverfish <vandras2003@gmail.com>
2020-08-25 10:10:41 +0200

    started working on debug template

---
+++ debug.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define ull unsigned long long
+#define ll long long
+#define pb push_back
+#define pii pair<int, int>
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define TC int tc; cin >> tc; while(tc--)
+
+struct debug{
+#ifdef _LOCAL
+	~debug(){ cerr << endl; }
+
+#else
+
+#endif
+};
+
+using namespace std;
+
+#define expand(...) " [" << #__VA_ARGS__ << " : " << (__VA_ARGS__) << " ]"
+
+const int INF = 1e9 + 7;
+
+int main(void)
+{
+	FAST;
+	int a = 10, b = 12;
+	int animal = 10, africa = 20;
+	cerr << expand(a)  << expand(b) << endl;
+
+	return 0;
+}
