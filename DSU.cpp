185445f9be47aaabaece58e333627284672cadec
s1lverfish <vandras2003@gmail.com>
2020-08-31 11:58:58 +0200

    new DSU template

--- DSU.cpp
+++ DSU.cpp
@@ -1,74 +1,80 @@
 #include <bits/stdc++.h>
-
-#define ull unsigned long long
-#define ll long long
-#define pb push_back
-#define pii pair<int, int>
-#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define TC int tc; cin >> tc; while(tc--)
-
 using namespace std;
 
-#ifndef ONLINE_JUDGE
-#  define LOG(x) (cerr << #x << " = " << (x) << endl)
+/*<DEBUG>*/
+#define tem template <typename 
+#define can_shift(_X_, ...) enable_if_t<sizeof test<_X_>(0) __VA_ARGS__ 8, debug&> operator<<(T i)
+#define _op debug& operator<<
+tem C > auto test(C *x) -> decltype(cerr << *x, 0LL);
+tem C > char test(...);
+tem C > struct itr{C begin, end; };
+tem C > itr<C> get_range(C b, C e) { return itr<C>{b, e}; };
+struct debug{
+#ifdef _LOCAL
+	~debug(){ cerr << endl; }
+	tem T > can_shift(T, ==){ cerr << boolalpha << i; return *this; }
+	tem T> can_shift(T, !=){ return *this << get_range(begin(i), end(i)); }
+	tem T, typename U > _op (pair<T, U> i){ 
+		return *this << "< " << i.first << " , " << i.second << " >"; }
+	tem T> _op (itr<T> i){
+		*this <<  "{ ";
+		for(auto it = i.begin; it != i.end; it++){
+			*this << " , " + (it==i.begin?2:0) << *it;
+		}
+		return *this << " }";
+	}
 #else
-#  define LOG(x) ((void)0)
-#endif
+tem T> _op (const T&) { return *this; }
+#endif 
+};
+
+string _ARR_(int* arr, int sz){
+	string ret = "{ " + to_string(arr[0]); 
+	for(int i = 1; i < sz; i++) ret += " , " +  to_string(arr[i]);
+	ret += " }"; return ret;
+}
+
+#define exp(...) " [ " << #__VA_ARGS__ << " : " << (__VA_ARGS__) << " ]"
+/*</DEBUG>*/
+
+typedef int64_t ll;
+typedef uint64_t ull;
+typedef uint32_t uint;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define TC int __TC__; cin >> __TC__; while(__TC__--)
 
 const int INF = 1e9 + 7;
 
+struct DSU {
+    int p[MxN], sz[MxN], cmp;
 
-struct dsu 
-{
-	int size, numSets;
-	vector<int> rank, p, setSize;
+    void init(int n) {
+        iota(p, p + n, 0);
+        fill(sz, sz + n, 1);
+        cmp = n;
+    }
 
-	void init(int n) { size = n; numSets = n;
-		rank.assign(n, 0);
-		p.resize(n); iota(p.begin(), p.end(), 0);
-		setSize.assign(n, 1);
-	}
+    int operator[](int u) {
+        return (p[u] == u ? u : p[u] = (*this)[p[u]]);
+    }
 
-	void printParents(){ for(int i = 0; i < size; i++) cout << i << " : " << p[i] << endl; }
-	void printRank(){ for(int i = 0; i < size; i++) cout << i << " : " << rank[i] << endl; }
-
-	int findSet(int node){
-		int root = node; while(root != p[root]) root = p[root];
-		int nxt = node;	while(p[node] != root) { nxt = p[node]; p[node] = root; node = nxt; } 
-		return root;
-	}
-
-	bool isSameSet(int a, int  b){return findSet(a) == findSet(b);}
-	int numOfSets() { return numSets; }
-	int sizeOfSet(int a) { return setSize[findSet(a)]; }
-
-	void unionSet(int a, int b) {
-		if(!isSameSet(a,b)){	
-			numSets--;
-			a = findSet(a); b = findSet(b);
-			if(rank[a] > rank[b]) p[b] = a, setSize[a] += setSize[b];
-			else {	p[a] = b; setSize[b] += setSize[a];
-					if(rank[a] == rank[b]) rank[b]++; }
-		}
-	}
-};
+    void operator()(int u, int v) {
+        u = (*this)[u];
+        v = (*this)[v];
+        if (u == v) return;
+        if (sz[u] < sz[v]) swap(u, v);
+        p[v] = u;
+        sz[u] += sz[v];
+        cmp--;
+    }
+} dsu;
 
 int main(void)
 {
-	FAST
-	
-	dsu s;
-	s.init(8);
-	s.unionSet(0,1);
-	s.unionSet(2,3);
-	s.unionSet(4,5);
-	s.unionSet(6,7);
-	s.unionSet(0,2);
-	s.unionSet(4,6);
-	s.unionSet(6,2);
-
-	s.printRank();
-	cout << s.sizeOfSet(0) << endl;
+	FAST;
 
 	return 0;
 }
